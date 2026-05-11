0537e3127805d2e410343ba5f90350e5f347c9df
HanYunJi <hyj9908@gmail.com>
2026-05-11 09:51:51 +0900

    feat: backend start

---
+++ backend/src/main/java/com/smartfactory/scada/ScadaApplication.java
@@ -0,0 +1,13 @@
+package com.smartfactory.scada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScadaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/smartfactory/scada/ScadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartfactory.scada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
